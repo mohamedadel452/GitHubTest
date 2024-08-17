394f20ff0042964d2c32edbf5721852d0fdd92d3
mohamedadel452 <m7711843@gmail.com>
2024-08-17 14:30:17 +0300

    mohamed change main Activity

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.githubtest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "hello ", Toast.LENGTH_SHORT).show()
+
 
     }
 }
